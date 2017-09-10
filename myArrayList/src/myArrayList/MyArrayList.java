2f23f816d490f02291b8054d1d0d5a0792e8d546
Aravind <aravind@Aravinds-MacBook-Pro.local>
2017-09-09 22:46:57 -0400

    ant working version.

--- myArrayList/src/myArrayList/MyArrayList.java
+++ myArrayList/src/myArrayList/MyArrayList.java
@@ -1,26 +1,33 @@
+
+package myArrayList;
+
 import java.io.*;
 import java.util.*;
 
-public class MyArrayList {
-	private static int[] myArrayList = new int[50];
+public class MyArrayList{
+	private int[] myArrayList = new int[50];
 
-	private static void intializeArray(){
+	public MyArrayList(){
+		intializeArray();
+	}
+
+	private void intializeArray(){
 		for(int i=0; i < myArrayList.length; i++){
 			myArrayList[i] = -1;
 		}
 	}
 
-	public static void insertSorted(int newValue){
+	public void insertSorted(int newValue){
 		insertSorted(0, newValue);
 	}
 
-	private static void insertSorted(int i, int newValue){
+	private void insertSorted(int i, int newValue){
 		for(i=0; i < myArrayList.length; i++){
 			if(0 == i && -1 == myArrayList[i]){
 				myArrayList[i] = newValue;
 				break;
 			}
-			else if(-1 != myArrayList[i] && myArrayList[i] > newValue){
+			else if(-1 != myArrayList[i] && myArrayList[i]>newValue){
 				int tempValue = myArrayList[i]; 
 				myArrayList[i] = newValue;
 				insertSorted(i+1, tempValue);
@@ -31,20 +38,16 @@ public class MyArrayList {
 		}
 	}
 
-	public static void displayArray(){
-		for(int i=0; i<myArrayList.length; i++){
-			if(-1 != myArrayList[i]){
-				System.out.println(myArrayList[i]);
-			}
-			else{
-				break;
+	public void displayArray(boolean print){
+		if(print){
+			for(int i=0; i<myArrayList.length; i++){
+				if(-1 != myArrayList[i]){
+					System.out.println(myArrayList[i]);
+				}
+				else{
+					break;
+				}
 			}
 		}
 	}
-	
-	public static void main(String[] args) {
-		intializeArray();
-		insertSorted(5);
-		displayArray();
-  	}
 }
--- myArrayList/src/myArrayList/driver/Driver.java
+++ myArrayList/src/myArrayList/driver/Driver.java
@@ -1,12 +1,18 @@
 package myArrayList.driver;
 
+import myArrayList.MyArrayList;
+
 public class Driver 
 {
 
 	public static void main(String[] args) 
 	{
 	    // command line validation is missing here. FIXME!
-	    System.out.println("Hello World: " + args[0] + ", " + args[1] + ", " + args[2]);
+	    //System.out.println("Hello World: " + args[0] + ", " + args[1] + ", " + args[2]);
+		MyArrayList myArrayList = new MyArrayList();
+
+		myArrayList.insertSorted(5);
+		myArrayList.displayArray(true);
 	}
 	
 }
