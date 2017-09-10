06b1b67bf01e0ddef21518333a3a2a93ef8e043d
Aravind <aravind@Aravinds-MacBook-Pro.local>
2017-09-09 23:24:19 -0400

    ant working version.

--- myArrayList/src/myArrayList/driver/Driver.java
+++ myArrayList/src/myArrayList/driver/Driver.java
@@ -8,9 +8,28 @@ public class Driver
 	public static void main(String[] args) 
 	{
 	    // command line validation is missing here. FIXME!
-	    //System.out.println("Hello World: " + args[0] + ", " + args[1] + ", " + args[2]);
-		MyArrayList myArrayList = new MyArrayList();
+	    String pramStr = "", inputFile, outputFile;
+	    if(0!= args.length){
+	    	if(!args[0].equals("${arg0}")){
+	    		pramStr += args[0] + ", ";
+	    		inputFile = args[0];
+	    	}
+	    	else{
 
+	    	}
+	    	if(!args[1].equals("${arg1}")){
+				pramStr += args[1];
+				outputFile = args[1];
+	    	}
+	    	else{
+
+	    	}
+	    }
+	    if(pramStr != ""){
+	    	System.out.println("Hello World: " + pramStr);
+	    }
+
+		MyArrayList myArrayList = new MyArrayList();
 		myArrayList.insertSorted(5);
 		myArrayList.displayArray(true);
 	}
