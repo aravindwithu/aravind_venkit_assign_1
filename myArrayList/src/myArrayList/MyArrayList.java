4e5d734a548daa3368f84fe074f5df98bddb65ec
Aravind <aravind@Aravinds-MacBook-Pro.local>
2017-09-09 21:42:45 -0400

    added int array private data member

--- myArrayList/src/myArrayList/MyArrayList.java
+++ myArrayList/src/myArrayList/MyArrayList.java
@@ -16,21 +16,24 @@ public class MyArrayList {
 
 	private static void insertSorted(int i, int newValue){
 		for(i=0; i < myArrayList.length; i++){
-			if(i==0 && myArrayList[i] == -1){
+			if(0 == i && -1 == myArrayList[i]){
 				myArrayList[i] = newValue;
 				break;
 			}
-			else if(myArrayList[i] != -1 && myArrayList[i]>newValue){
+			else if(-1 != myArrayList[i] && myArrayList[i] > newValue){
 				int tempValue = myArrayList[i]; 
 				myArrayList[i] = newValue;
 				insertSorted(i+1, tempValue);
+			}
+			else if(-1 == myArrayList[i]){
+				myArrayList[i] = newValue;
 			}
 		}
 	}
 
 	public static void displayArray(){
 		for(int i=0; i<myArrayList.length; i++){
-			if(myArrayList[i] != -1){
+			if(-1 != myArrayList[i]){
 				System.out.println(myArrayList[i]);
 			}
 			else{
